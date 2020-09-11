6133b801a88caa867cd8580b66862f1f559732b6
valdir-ti <valdir.ti@gmail.com>
2020-09-11 12:23:40 -0300

    Criação da tela de introdução ao aplicativo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 import ChatListItem from './components/ChatListItem';
+import ChatIntro from './components/ChatIntro';
 
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
@@ -48,7 +49,7 @@ export default () => {
 
       </div>
       <div className="content-area">
-        Conteúdo  
+          <ChatIntro />
       </div>    
     </div>
   )
---
+++ src/components/ChatIntro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ChatIntro.css';
+
+export default () =>{
+  return (
+    <div className="chat-intro">
+      <img src="https://web.whatsapp.com/img/intro-connection-light_c98cc75f2aa905314d74375a975d2cf2.jpg" alt=""/>
+      <h1>Mantenha seu celular conectado</h1>
+      <h2>O WhatsApp conecta ao seu telefone para sincronizar as suas mensagens.<br/>Para reduzir o uso de dados, conecte o seu telefone a uma rede Wi-fi.</h2>
+    </div>
+  )  
+};
